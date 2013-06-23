b7338b42bed4e1cf3d24da7b6e6bf186991c8745
Vínicius Oliveira <oliveira.mvs@gmail.com>
2013-06-23 10:23:30 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VinoliveraCom::Application.config.secret_token = '5d21063df5ddc2c3075de54fb351888495dcf3d3769f8834da5edb0ecf9632d2eb78f832484ffa1a55ab1e954d07f7c24adf9e7958f82f0439284447bdd80805'
