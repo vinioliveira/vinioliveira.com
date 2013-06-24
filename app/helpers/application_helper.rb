9c228f02c4891de38d950718b32a9d31bb6d07f8
Vínicius Oliveira <oliveira.mvs@gmail.com>
2013-06-23 23:23:44 -0300

    Improving controller assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def controller_stylesheet_link_tag
+    stylesheet = "#{params[:controller]}.css" #e.g. home_controller =>assets/stylesheets/home.css
+    #if stylesheet asset exists include it
+    unless Rails.application.assets.find_asset(stylesheet).nil?
+      stylesheet_link_tag stylesheet
+    end
+  end
+  def controller_javascript_include_tag
+    javascript = "#{params[:controller]}.js" #e.g. home_controller =>assets/javascripts/home.js
+    unless Rails.application.assets.find_asset(javascript).nil?
+      javascript_include_tag javascript
+    end
+  end
 end
